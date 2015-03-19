451bbbac4542a4baec8cf1029b8a01c6b449a70b
bibiana <bcristofol@gmail.com>
2015-03-19 21:18:14 +0000

    Fix bug in selectorTag with length 2

--- Answer.js
+++ Answer.js
@@ -7,9 +7,27 @@ var $ = function (selector) {
   var idVar = document.getElementById(selectorNew[1]);
   var classVar = document.getElementsByClassName(selectorNew[1]);
 
+  var selectorParts = selector.split(/(?=[#\.])/);
+
   var selectorId = selector.charAt(0) === '#';
   var selectorClass = selector.charAt(0) === '.';
   var selectorTag = !selectorId && !selectorClass;
+
+  var getSelectorId = function() {
+    for (var i = 0; i < selectorParts.length; i++) {
+      if (selectorParts[i].charAt(0) === '#') {
+        return selectorParts[i].substring(1, selectorParts[i].length);
+      }
+    }
+  }
+
+  var getSelectorClass = function() {
+    for (var i = 0; i < selectorParts.length; i++) {
+      if (selectorParts[i].charAt(0) === '.') {
+        return selectorParts[i].substring(1, selectorParts[i].length);
+      }
+    }
+  }
 
   var addAllElementsFromArrayToNewArray = function(array) {
     for (var i = 0; i < array.length; i++) {
@@ -17,9 +35,11 @@ var $ = function (selector) {
     }
   }
 
-  var addElementsWithClassOrIdFromArrayToNewArray = function(array) {
+  var selectByClassOrId = function(sid, sclass, array) {
     for (var i = 0; i < array.length; i++) {
-      if (array[i].className.includes(selectorNew[1]) || array[i].id.includes(selectorNew[1])){
+      classParts = array[i].className.split(" ");
+      if ((classParts != undefined && classParts.indexOf(sclass) !== -1) ||
+          (sid != undefined && array[i].id === sid)) {
         elements.push(array[i]);
       }
     } 
@@ -31,7 +51,7 @@ var $ = function (selector) {
         attributeId = array[i].getAttribute('id');
         attributeClass = array[i].getAttribute('class');
         if (attributeClass != undefined) {
-          attributeClassNew = attributeClass.split(' ');
+          attributeClassNew = attributeClass.split(' '); // could have more than two class values
           if (selector.includes(attributeId) && (selector.includes(attributeClassNew[0]) || selector.includes(attributeClassNew[1]))) {
             elements.push(array[i]);
           }
@@ -40,24 +60,18 @@ var $ = function (selector) {
     }
   }
 
+  var sid = getSelectorId(); // may be undefined
+  var sclass = getSelectorClass(); // may be undefined
+
   if (selectorTag) {
-    if (selectorNew.length === 1) {
-      addAllElementsFromArrayToNewArray(tagVar);
-    }
-    else if (selectorNew.length === 2) {
-      addElementsWithClassOrIdFromArrayToNewArray(tagVar);
-    }
-    else if (selectorNew.length === 3) {
-      addElementsWithClassAndIdFromArrayToNewArray(tagVar);
-    }
+    if (selectorNew.length === 1) { addAllElementsFromArrayToNewArray(tagVar); }
+    else if (selectorNew.length === 2) { selectByClassOrId(sid, sclass, tagVar); }
+    else if (selectorNew.length === 3) { addElementsWithClassAndIdFromArrayToNewArray(tagVar); }
   }
   else if (selectorId) {
-    if (idVar != undefined) {
-      elements.push(idVar);
-    }
+    if (idVar != undefined) { elements.push(idVar); }
   }
-  else if (selectorClass) {
-    addAllElementsFromArrayToNewArray(classVar);
-  }
+  else if (selectorClass) { addAllElementsFromArrayToNewArray(classVar); }
+
   return elements;
 };
